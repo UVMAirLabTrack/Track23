9ebf3577510990e0eb89f5e2444ce284d02ea439
IvanTheTerrier <112969078+iantheterror@users.noreply.github.com>
2023-11-15 19:22:52 -0500

    k

--- CoreRaspberry/ros_ws/four_way_light/setup.py
+++ CoreRaspberry/ros_ws/four_way_light/setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup
 
-package_name = 'four_way_light'
+package_name = 'four_way_stoplight'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[],
     py_modules=[
-        'four_way_light.ESP_serial',  # Replace with your actual module name
+        'four_way_stoplight.serial_com',
+        'four_way_stoplight.pub',
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Ian M',
     maintainer_email='iantheterror4@gmail.com',
-    description='Control LED Stoplight via ROS 2',
+    description='ROS 2 package for controlling a four-way stoplight using ESP serial communication',
     license='TODO',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'ESP_serial = four_way_light.ESP_serial:main',
+            'ESP_serial = four_way_stoplight.serial_com:main',
+            'Light_pub = four_way_stoplight.pub:main',
         ],
     },
 )
