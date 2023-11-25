ddf2395301c164d9e4703ed7d48e0b84c5937bae
iantheterror <112969078+iantheterror@users.noreply.github.com>
2023-11-24 19:07:12 -0500

    added test variable

--- CoreRaspberry/src/world_gen/world_gen/test.py
+++ CoreRaspberry/src/world_gen/world_gen/test.py
@@ -1,6 +1,6 @@
 import rclpy
 from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Pose, Point, Vector3, Quaternion, ColorRGBA
+from geometry_msgs.msg import Pose
 import os
 
 def world_select(file_path):
@@ -59,17 +59,20 @@ def main():
     for key, value in pose_data.items():
         node.get_logger().info(f"Pose data: {key} - {value}")
 
+    testvar = pose_data.get('Z_position', 0.0)
+    node.get_logger().info(f"testVar: {testvar}")
+
     # Set marker properties using the extracted pose data
-    marker.pose.position = Point(x=pose_data.get('X_position', 0.0),
-                                y=pose_data.get('Y_position', 0.0),
-                                z=pose_data.get('Z_position', 0.0))
-    marker.scale = Vector3(x=pose_data.get('Scale_x', 1.0),
-                        y=pose_data.get('Scale_y', 1.0),
-                        z=pose_data.get('Scale_z', 1.0))
-    marker.color = ColorRGBA(r=pose_data.get('Color_r', 1.0),
-                            g=pose_data.get('Color_g', 1.0),
-                            b=pose_data.get('Color_b', 1.0),
-                            a=pose_data.get('Color_a', 1.0))
+    marker.pose.position.x = pose_data.get('X_position', 0.0)
+    marker.pose.position.y = pose_data.get('Y_position', 0.0)
+    marker.pose.position.z = pose_data.get('Z_position', 0.0)
+    marker.scale.x = pose_data.get('Scale_x', 1.0)
+    marker.scale.y = pose_data.get('Scale_y', 1.0)
+    marker.scale.z = pose_data.get('Scale_z', 1.0)
+    marker.color.r = pose_data.get('Color_r', 1.0)
+    marker.color.g = pose_data.get('Color_g', 1.0)
+    marker.color.b = pose_data.get('Color_b', 1.0)
+    marker.color.a = pose_data.get('Color_a', 1.0)
     marker.lifetime.sec = int(pose_data.get('Lifetime_sec', 0))
     marker.frame_locked = bool(pose_data.get('Frame_locked', False))
     marker.mesh_use_embedded_materials = bool(pose_data.get('Mesh_use_embedded_materials', True))
