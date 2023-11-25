0a5068856e6aadc3209ff96d6c08c22e744d4414
iantheterror <112969078+iantheterror@users.noreply.github.com>
2023-11-24 19:10:04 -0500

    using testvar to shift values

--- CoreRaspberry/src/world_gen/world_gen/test.py
+++ CoreRaspberry/src/world_gen/world_gen/test.py
@@ -65,7 +65,7 @@ def main():
     # Set marker properties using the extracted pose data
     marker.pose.position.x = pose_data.get('X_position', 0.0)
     marker.pose.position.y = pose_data.get('Y_position', 0.0)
-    marker.pose.position.z = pose_data.get('Z_position', 0.0)
+    marker.pose.position.z = testvar
     marker.scale.x = pose_data.get('Scale_x', 1.0)
     marker.scale.y = pose_data.get('Scale_y', 1.0)
     marker.scale.z = pose_data.get('Scale_z', 1.0)
