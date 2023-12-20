c09fd2e45728defccb410f970f9c120b1741eaa8
iantheterror <112969078+iantheterror@users.noreply.github.com>
2023-12-19 19:59:41 -0500

    Added control for second pair

--- CoreRaspberry/src/four_way_light/four_way_light/direct_light.py
+++ CoreRaspberry/src/four_way_light/four_way_light/direct_light.py
@@ -3,12 +3,17 @@ from std_msgs.msg import Int32MultiArray
 
 def map_state_to_integer(state):
     state_mapping = {
-        "Red": 1,
-        "Yellow": 2,
-        "Green": 3,
-        "Right": 4,
-        "Left": 5,
-        "All": 10
+        "off": 0,
+        "red": 1,
+        "yellow": 2,
+        "green": 3,
+        "right": 4,
+        "left": 5,
+        "go-R": 6,
+        "go-l": 7,
+        "stop-r": 8,
+        "stop-l": 9,
+        "all": 10,
     }
     return state_mapping.get(state, 0)
 
@@ -20,18 +25,21 @@ def main():
     publisher = node.create_publisher(Int32MultiArray, 'four_way_state', 10)
 
     while rclpy.ok():
-        user_input = input("Enter state (Red, Yellow, Green, Right, Left, All) or an integer: ")
+        user_input = input("Enter state 1 (Red, Yellow, Green, Right, Left, All) or an integer: ")
+        user_input2 = input("Enter state 2 (Red, Yellow, Green, Right, Left, All) or an integer: ")
 
         try:
             # Try parsing input as an integer
             state_integer = int(user_input)
+            state_integer2 = int(user_input2)
         except ValueError:
             # If it's not a valid integer, map the input to an integer state
             state_integer = map_state_to_integer(user_input)
+            state_integer2 = map_state_to_integer(user_input2)
 
         # Create an array message with two integers
         msg = Int32MultiArray()
-        msg.data = [state_integer, 0]  # For simplicity, setting the second element to 0
+        msg.data = [state_integer, state_integer2]  # For simplicity, setting the second element to 0
 
         node.get_logger().info(f'Publishing: {msg.data}')
         publisher.publish(msg)
