9d01f6d6a7d0497975b2b08cb9b24a21f85eb823
IvanTheTerrier <112969078+iantheterror@users.noreply.github.com>
2024-02-09 12:43:19 -0500

    Update ESPnowSender.ino

--- InteractiveElements/Base Files/ESPnowSender/ESPnowSender.ino
+++ InteractiveElements/Base Files/ESPnowSender/ESPnowSender.ino
@@ -47,8 +47,8 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void setup() {
-  //Serial.begin(115200);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  //Serial.begin(9600);
   Serial.setTimeout(50);
  
   WiFi.mode(WIFI_STA);
