be01491842f897fc6f829092c0f454c03da812ab
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-02-14 23:33:49 -0500

    try again

--- CoreRaspberry/src/world_gen/setup.py
+++ CoreRaspberry/src/world_gen/setup.py
@@ -22,7 +22,7 @@ setup(
     license='TODO',
     data_files=[
         (os.path.join('share', package_name, 'worlds'), glob(os.path.join('src', package_name, 'worlds', '*')))
-        (os.path.join('share', package_name, 'markers'), glob(os.path.join('src', package_name, 'markers', '*')))
+        #(os.path.join('share', package_name, 'markers'), glob(os.path.join('src', package_name, 'markers', '*')))
     ],
     tests_require=['pytest'],
     entry_points={
