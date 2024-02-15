329a25d565196fbf4615fe0975f8f82d3b6c5621
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-02-14 22:21:38 -0500

    last try?

--- CoreRaspberry/src/world_gen/setup.py
+++ CoreRaspberry/src/world_gen/setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 from glob import glob
 import os
 
-
 package_name = 'world_gen'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    package_dir={package_name: 'src/' + package_name + '/world_gen'},  # Adjust this line
     py_modules=[
-        'world_gen.gen',
-        'world_gen.FourWayViz',
-        'world_gen.color_publisher',
+        'gen',
+        'FourWayViz',
+        'color_publisher',
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,8 +20,6 @@ setup(
     maintainer_email='iantheterror4@gmail.com',
     description='ROS 2 package for generating world scenarios in RVIZ',
     license='TODO',
-
-    package_dir={package_name: 'src/' + package_name},
     data_files=[
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'worlds'), glob('src/' + package_name + '/worlds/*')),
@@ -29,9 +27,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'gen = world_gen.gen:main',
-            'fourwayviz = world_gen.FourWayViz:main',
-            'ct = world_gen.color_publisher:main',
+            'gen = world_gen.world_gen.gen:main',  # Adjust these lines
+            'fourwayviz = world_gen.world_gen.FourWayViz:main',
+            'ct = world_gen.world_gen.color_publisher:main',
         ],
     },
 )
