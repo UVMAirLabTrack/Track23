520b77536b40c1ff3457b368230946bf89883ba2
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-02-16 20:37:14 -0500

    ...

--- CoreRaspberry/src/world_gen/world_gen/four_way_marker.py
+++ CoreRaspberry/src/world_gen/world_gen/four_way_marker.py
@@ -99,7 +99,15 @@ class FourWayVisualizer(Node):
             else:
                 # If there are not enough colors in the received list, default to white
                 self.light_colors[light_name] = 'white'
-
+                
+        # Add the following loop to update other markers
+        for other_marker in ['light_a', 'light_b', 'light_c', 'light_d']:
+            other_light_name = f'four_way_marker_{other_marker}'
+            if other_light_name in self.light_colors and colors:
+                self.light_colors[other_light_name] = colors.pop(0)
+            else:
+                self.light_colors[other_light_name] = 'white'
+                
     def publish_marker(self):
         marker_msg = Marker()
         marker_msg.header.frame_id = 'map'  # Set the frame ID as needed
