69bafdb290c4069ea91537f4b4a425101b83ebfa
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-02-16 20:41:36 -0500

    updated publish speed

--- CoreRaspberry/src/world_gen/world_gen/color_publisher.py
+++ CoreRaspberry/src/world_gen/world_gen/color_publisher.py
@@ -7,7 +7,7 @@ class ColorPublisher(Node):
     def __init__(self):
         super().__init__('color_publisher')
         self.publisher_ = self.create_publisher(String, 'four_way_state', 10)
-        self.timer_ = self.create_timer(3.0, self.timer_callback)
+        self.timer_ = self.create_timer(1.0, self.timer_callback)
         self.colors = [['red', 'yellow', 'green', 'white'],
                        ['blue', 'red', 'white', 'green'],['green','blue', 'red', 'white']]  # Add more color sequences if needed
         self.num_lights = 4
