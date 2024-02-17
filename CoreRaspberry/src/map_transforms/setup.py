7e14e8f8ea45c636fb3581409d4a6c8732773b49
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-02-17 11:00:24 -0500

    added map transforms

---
+++ CoreRaspberry/src/map_transforms/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'world_gen'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    #package_dir={package_name: 'src/' + package_name},
+    py_modules=[
+    'world_gen.gen',
+    'world_gen.four_way_marker',
+    #'world_gen.color_publisher',
+    
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ian M',
+    maintainer_email='iantheterror4@gmail.com',
+    description='ROS 2 package for generating world scenarios in RVIZ',
+    license='TODO',
+    data_files=[
+    ('share/' + package_name + '/worlds', glob(os.path.join('world_gen', 'worlds', '*.*'))),
+    ('share/' + package_name + '/markers', glob(os.path.join('world_gen', 'markers', '*.*'))),
+    ('share/' + package_name, ['package.xml']),
+    ],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'gen = world_gen.gen:main',
+            'four_way_marker = world_gen.four_way_marker:main',
+            #'color_test = world_gen.color_publisher:main',
+        ],
+    },
+)
