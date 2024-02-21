823231c2f523c1ff3882fd7fc659bc56740ccaf0
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-02-20 20:05:36 -0500

    more launch files

--- CoreRaspberry/src/world_gen/launch/world_launch_circle.py
+++ CoreRaspberry/src/world_gen/launch/world_launch_circle.py
@@ -5,31 +5,19 @@ def generate_launch_description():
    return LaunchDescription([
       
     Node(
-    package='world_gen',
+    package='testing_pubs',
     
-    executable='gen',
-    name='world_map'
+    executable='circle_odometry',
+    name='drive_in_circles'
 ),
 
-    Node(
-    package='world_gen',
-    
-    executable='four_way_marker',
-    name='Four_markers'
-),
+
 
     Node(
     package='four_way_light',
     
     executable='pub',
-    name='four_way_control'
-),
+    name='four_way_control')
 
-    Node(
-    package='map_transforms',
-    
-    executable='odom_to_map',
-    name='odom_transform'
-),
 
    ])
