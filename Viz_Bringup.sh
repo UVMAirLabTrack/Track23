7299bf9e60fcdb4428a835938daf216c47dc3fb8
IvanTheTerrier <112969078+iantheterror@users.noreply.github.com>
2024-03-06 22:16:01 -0500

    rw nor rm

--- Viz_Bringup.sh
+++ Viz_Bringup.sh
@@ -4,7 +4,7 @@ docker run -it --rm \
 --net=host \
 --env="DISPLAY" \
 --env="QT_X11_NO_MITSHM=1" \
--v /tmp/.X11-unix:/tmp/.X11-unix:rm \
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 --device=/dev/dri:/dev/dri \
 --security-opt apparmor:unconfined \
 raspberrycore:latest
