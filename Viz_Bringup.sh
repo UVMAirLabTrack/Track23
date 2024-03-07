02d433bddd1c9c86702ec4e2d97759cd3261a65e
IvanTheTerrier <112969078+iantheterror@users.noreply.github.com>
2024-03-06 22:22:13 -0500

    added ports

--- Viz_Bringup.sh
+++ Viz_Bringup.sh
@@ -6,5 +6,7 @@ docker run -it --rm \
 --env="QT_X11_NO_MITSHM=1" \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 --device=/dev/dri:/dev/dri \
+-p 9090:9090 \
+-p 8888:8888 \
 --security-opt apparmor:unconfined \
 raspberrycore:latest
