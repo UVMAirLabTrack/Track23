330abfb0037e69c1728b14bfe08397262bf30eda
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-03-14 11:49:42 -0400

    attempt 2

--- CoreRaspberry/src/world_gen/setup.py
+++ CoreRaspberry/src/world_gen/setup.py
@@ -14,7 +14,7 @@ setup(
     'world_gen.four_way_marker',
     'world_gen.parse_and_pass',
     'world_gen.gen_v2',
-    'core_functions.open_world_data'
+    'world_gen.open_world_data'
     
     ],
     install_requires=['setuptools','core_functions'],
