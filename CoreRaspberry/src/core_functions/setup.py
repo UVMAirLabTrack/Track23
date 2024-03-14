4ba73959fd5b5752fbe9dfb9975906cfc01d836f
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-03-14 10:10:51 -0400

    trying to make a second package

---
+++ CoreRaspberry/src/core_functions/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'four_way_light'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[],
+    py_modules=[
+        'four_way_light.pub',
+
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ian M',
+    maintainer_email='iantheterror4@gmail.com',
+    description='ROS 2 package for controlling a four-way stoplight using ESP serial communication',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pub = four_way_light.pub:main',
+        ],
+    },
+)
