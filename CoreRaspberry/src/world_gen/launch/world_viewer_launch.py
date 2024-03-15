9a033c30ba5a734c288a7f997d77c7854f866c36
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-03-15 17:23:46 -0400

    oops

--- CoreRaspberry/src/world_gen/launch/world_viewer_launch.py
+++ CoreRaspberry/src/world_gen/launch/world_viewer_launch.py
@@ -8,7 +8,7 @@ import os
 def generate_launch_description():
     package_share_directory = get_package_share_directory('world_gen')
     rviz_directory = os.path.join(package_share_directory, 'world_gen', 'rviz')
-    file = 'config1.rviz',
+    file = 'config1.rviz'
     path = os.path.join(rviz_directory,file)
 
     # Declare a launch argument for the RViz configuration file parameter
