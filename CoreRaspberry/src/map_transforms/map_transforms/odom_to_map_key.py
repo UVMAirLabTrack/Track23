4db6cbdb4b32225fc00b23734a2b9a6a0f368e17
iantheterror <112969078+iantheterror@users.noreply.github.com>
2024-04-18 14:31:39 -0400

    still updating odom

--- CoreRaspberry/src/map_transforms/map_transforms/odom_to_map_key.py
+++ CoreRaspberry/src/map_transforms/map_transforms/odom_to_map_key.py
@@ -120,9 +120,9 @@ class OdomTransformer(Node):
     def transform_odom(self, odom_msg):
        
         world_yaw = 0.0  #no X and Y angles.  Need to pull these frames from the custom pose stuff
-        world_x = self.saved_odom.pose.pose.position.x
-        world_y = self.saved_odom.pose.pose.position.y
-        world_z = self.saved_odom.pose.pose.position.z
+        world_x = self.marker.pose.position.x
+        world_y = self.marker.pose.position.y
+        world_z = self.marker.pose.position.z
 
         car_y_shift = math.pi/2
 
